82b6bd510abb167a92cdc54b42a04fa58a91ee30
Kristian2109 <kristian.petrov@gmail.com>
2024-08-04 11:19:20 +0000

    Add middeware to the app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, json } from 'express';
 import dotenv from 'dotenv';
+import { urlencoded } from 'body-parser';
 dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT) || 8000;
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
