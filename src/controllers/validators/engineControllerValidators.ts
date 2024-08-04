d702daaf95c0f29c503ff27a1509ea7726338e73
Kristian2109 <kristian.petrov@gmail.com>
2024-08-04 12:06:29 +0000

    Add controllers

--- src/controllers/validators/engineControllerValidators.ts
+++ src/controllers/validators/engineControllerValidators.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import z from 'zod';
 
-export function worflowIdQueryValidator(req: Request, res: Response, next: NextFunction) {
+export function workflowIdQueryValidator(req: Request, res: Response, next: NextFunction) {
   const workflowId = req.query.workflowId;
   z.string().uuid().safeParse(workflowId);
   next();
