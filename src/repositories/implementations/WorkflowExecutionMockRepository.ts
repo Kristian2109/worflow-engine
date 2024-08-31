b428f76d5387540aa578709d419bdbab8f5141e8
Kristian2109 <kristian.petrov@gmail.com>
2024-08-31 16:29:40 +0000

    Fix executor creatiom

--- src/repositories/implementations/WorkflowExecutionMockRepository.ts
+++ src/repositories/implementations/WorkflowExecutionMockRepository.ts
@@ -2,11 +2,17 @@ import { randomUUID, UUID } from "crypto";
 import WorkflowDefinition from "../../workflowDefinition/definitions/WorkflowDefinition";
 import WorkflowExecutor from "../../workflowExecution/executors/WorkflowExecutor";
 import WorkflowExecutionRepository from "../interfaces/WorkflowExecutionRepository";
+import ExecutionState from "../../workflowExecution/state/ExecutionState";
+import { ExecutionStatus } from "../../workflowExecution/state/ExecutionStatus";
 
 export default class WorkflowExecutionMockRepository implements WorkflowExecutionRepository {
   private executions: Map<UUID, WorkflowExecutor> = new Map();
   async createExecution(definition: WorkflowDefinition): Promise<WorkflowExecutor> {
-    const newExecution = new WorkflowExecutor(randomUUID(), definition)
+    const newExecution = new WorkflowExecutor(
+      randomUUID(), 
+      definition, 
+      new ExecutionState(randomUUID(), definition.id, new Map(), ExecutionStatus.Executing),
+    )
     this.executions.set(newExecution.id, newExecution);
     return newExecution;
   }
