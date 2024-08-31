61dc9a9ca0850125671e29d9054c89821fe042ea
Kristian2109 <kristian.petrov@gmail.com>
2024-08-31 16:11:07 +0000

    Add step state to execution state

--- src/workflowExecution/state/ExecutionState.ts
+++ src/workflowExecution/state/ExecutionState.ts
@@ -1,6 +1,10 @@
 import { UUID } from "crypto";
+import StepExecutionState from "./StepExecutionState";
 
 export default class ExecutionState {
+  public get stepStates(): Map<UUID, StepExecutionState> {
+    return this._stepStates;
+  }
   public get workflowDefinitionId(): UUID {
     return this._workflowDefinitionId;
   }
@@ -10,5 +14,14 @@ export default class ExecutionState {
   constructor(
     private readonly _id: UUID,
     private readonly _workflowDefinitionId: UUID,
+    private readonly _stepStates: Map<UUID, StepExecutionState>,
   ) {}
+
+  public getStepStateById(id: UUID): StepExecutionState {
+    const found = this._stepStates.get(id);
+    if (!found) {
+      throw new Error(`Step with ${id} not set`)
+    }
+    return found;
+  }
 }
